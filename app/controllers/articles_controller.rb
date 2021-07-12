162f2c0698021180ac0676aee842af486a47646c
takahiro-shimizu <tkbchando@gmail.com>
2021-07-12 17:24:34 +0000

    [Update]会員のShowページで投稿記事一覧を表示

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,7 +18,23 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to article_path(@article.id)
+    else
+      render :edit
+    end
+  end
+
   def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
   end
 
   private
--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -2,6 +2,7 @@ class MembersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @articles = Article.where(user_id: @user.id).page(params[:page]).per(5)
   end
 
   def edit
